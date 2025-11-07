b287d72a97dbe2684615a7601765f684ec534512
Tu Nombre <209029025+MateoAlRen@users.noreply.github.com>
2025-11-07 06:30:45 -0500

    CodeUp

---
+++ src/test/java/com/codeup/CodeUp/CodeUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.CodeUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
